576e89d41fa603330fe96fd00de7b9681da3f76d
Davi Silva <davisilvaphoto@gmail.com>
2019-11-04 13:44:49 -0300

    User entity and resource

---
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.educandoweb.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Davi", "davi@email.com", "9999 8888", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+}
